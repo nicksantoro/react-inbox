29e72ab9b3a7b3ac61c5a9ceb9d0c3b6107de766
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-11-04 22:49:31 -0800

    created components, state, props

---
+++ src/Components/Message.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Message = ({message}) => {
+    let labelList = message.labels.map( (label, index) => {
+        return (
+            <span className="label label-warning">{label}</span>
+
+        )
+    })
+
+    return (
+    <div 
+        className={`row message ${message.read ? 'read' : 'unread'} ${ message.selected ? 'selected' : ''} `
+    }>
+        
+        <div className="col-xs-1">
+            <div className="row">
+            <div className="col-xs-2">
+                <input 
+                    type="checkbox" 
+                    checked={ message.selected }
+                />
+            </div>
+            <div className="col-xs-2">
+                <i 
+                    className={`star fa fa-star${ message.starred ? '' : '-o'}`}
+                ></i>
+            </div>
+            </div>
+        </div>
+
+        <div className="col-xs-11">
+            
+            {labelList}
+            <a href="#">
+            Here is some message text that has a bunch of stuff
+            </a>
+        </div>
+    </div>
+    )
+}
+
+
+export default Message
---
+++ src/Components/MessageList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Message from './Message'
+
+class MessageList extends Component {
+
+    render() {
+        let messageList = this.props.messages.map( (message, index) => {
+            return (
+                <Message key={index} message={ message }/>
+            )
+        } )
+
+        return (
+            <div>
+                { messageList }
+            </div>
+        )
+    }
+}
+
+export default MessageList
